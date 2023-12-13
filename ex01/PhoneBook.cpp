890c2a361e989ad46eb02a04535f88d93fcacdb6
Teodor Taneski Feldschau <ttaneski@studnet.42berlin.de>
2023-12-13 15:17:35 +0100

    contact created

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,14 +6,15 @@
 /*   By: ttaneski <ttaneski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 14:39:14 by ttaneski          #+#    #+#             */
-/*   Updated: 2023/12/13 13:22:21 by ttaneski         ###   ########.fr       */
+/*   Updated: 2023/12/13 15:13:44 by ttaneski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
-Contact::Contact(std::string first_name, std::string last_name,
-	std::string nickname, std::string darkest_secret, std::string phone_number)
+Contact::Contact(std::string first_name = "", std::string last_name = "",
+	std::string nickname = "", std::string darkest_secret = "",
+	std::string phone_number = "")
 {
 	this->first_name = first_name;
 	this->last_name = last_name;
@@ -26,8 +27,29 @@ Contact::~Contact(void)
 {
 	std::cout << "Contact destroyed" << std::endl;
 }
+void Contact::createContact()
+{
+	std::cout << "Enter First Name" << std::endl;
+	std::cin >> first_name;
+	std::cout << "Enter Last Name" << std::endl;
+	std::cin >> last_name;
+	std::cout << "Enter Nickname" << std::endl;
+	std::cin >> nickname;
+	std::cout << "Enter Darkest Secret" << std::endl;
+	std::cin >> darkest_secret;
+	std::cout << "Enter Phone Number" << std::endl;
+	std::cin >> phone_number;
+}
 
-PhoneBook::PhoneBook(void)
+void Contact::printContact()
+{
+	std::cout << first_name << std::endl;
+	std::cout << last_name << std::endl;
+	std::cout << nickname << std::endl;
+	std::cout << darkest_secret << std::endl;
+	std::cout << phone_number << std::endl;
+}
+/* PhoneBook::PhoneBook(void)
 {
 	std::cout << "Contact created" << std::endl;
 }
@@ -36,11 +58,12 @@ PhoneBook::~PhoneBook(void)
 {
 	std::cout << "Contact destroyed" << std::endl;
 }
-
+ */
 int	main(int argc, char **argv)
 {
-	PhoneBook	PhoneBook;
+	Contact	Contact;
 
+	// PhoneBook	PhoneBook;
 	std::string str;
 	(void)argv;
 	if (argc == 1)
@@ -50,7 +73,11 @@ int	main(int argc, char **argv)
 			std::cout << "Enter input: ADD, SEARCH or EXIT" << std::endl;
 			std::cin >> str;
 			if (str == "ADD" || str == "add")
-				std::cout << std::setw(10) << str << std::endl;
+			{
+				Contact.createContact();
+				Contact.printContact();
+				// std::cout << std::setw(10) << str << std::endl;
+			}
 			else if (str == "SEARCH" || str == "search")
 				std::cout << str << std::endl;
 			else if (str == "EXIT" || str == "exit")
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,36 +6,41 @@
 /*   By: ttaneski <ttaneski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 14:39:17 by ttaneski          #+#    #+#             */
-/*   Updated: 2023/12/13 13:22:27 by ttaneski         ###   ########.fr       */
+/*   Updated: 2023/12/13 15:13:53 by ttaneski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
-#include <iostream>
-#include <iomanip>
+# include <iomanip>
+# include <iostream>
 
 class Contact
 {
-  public:
+  private:
 	std::string first_name;
 	std::string last_name;
 	std::string nickname;
 	std::string darkest_secret;
 	std::string phone_number;
+
+  public:
 	Contact(std::string first_name, std::string last_name, std::string nickname,
 		std::string darkest_secret, std::string phone_number);
+	void createContact();
+	void printContact();
+
 	~Contact(void);
 };
 
-class PhoneBook
+/* class PhoneBook
 {
   public:
 	int contactCount;
 	Contact Contact[8];
 	PhoneBook(void);
 	~PhoneBook(void);
-};
+}; */
 
 #endif
