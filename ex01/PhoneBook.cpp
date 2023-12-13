5743247a7c858fa8912a77924ae561c6e8e9c6a0
Teodor Taneski Feldschau <ttaneski@studnet.42berlin.de>
2023-12-13 14:01:55 +0100

    update

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -1,61 +1,66 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttaneski <ttaneski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 14:39:14 by ttaneski          #+#    #+#             */
-/*   Updated: 2023/12/12 16:02:56 by ttaneski         ###   ########.fr       */
+/*   Updated: 2023/12/13 13:22:21 by ttaneski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "phonebook.hpp"
+#include "PhoneBook.hpp"
 
-Contact::Contact(std::string first_name, std::string last_name, std::string nickname, std::string darkest_secret, int phone_number)
+Contact::Contact(std::string first_name, std::string last_name,
+	std::string nickname, std::string darkest_secret, std::string phone_number)
 {
-	// std::cout << "Contact created" << std::endl;
 	this->first_name = first_name;
 	this->last_name = last_name;
 	this->nickname = nickname;
 	this->darkest_secret = darkest_secret;
 	this->phone_number = phone_number;
-
 }
 
 Contact::~Contact(void)
 {
-	// std::cout << "Contact destroyed" << std::endl;
+	std::cout << "Contact destroyed" << std::endl;
 }
 
 PhoneBook::PhoneBook(void)
 {
-	// std::cout << "Contact created" << std::endl;
+	std::cout << "Contact created" << std::endl;
 }
 
 PhoneBook::~PhoneBook(void)
 {
-	// std::cout << "Contact destroyed" << std::endl;
+	std::cout << "Contact destroyed" << std::endl;
 }
+
 int	main(int argc, char **argv)
 {
-	Contact		first;
 	PhoneBook	PhoneBook;
 
 	std::string str;
 	(void)argv;
-	std::cout << "Enter input; ADD, SEARCH or EXIT" << std::endl;
 	if (argc == 1)
 	{
-		std::cin >> str;
-		if (str == "ADD")
-			std::cout << str << std::endl;
-		else if (str == "SEARCH")
-			std::cout << str << std::endl;
-		else if (str == "EXIT")
-			std::cout << str << std::endl;
-		else
-			std::cout << "Type ADD, SEARCH or EXIT" << std::endl;
+		while (1)
+		{
+			std::cout << "Enter input: ADD, SEARCH or EXIT" << std::endl;
+			std::cin >> str;
+			if (str == "ADD" || str == "add")
+				std::cout << std::setw(10) << str << std::endl;
+			else if (str == "SEARCH" || str == "search")
+				std::cout << str << std::endl;
+			else if (str == "EXIT" || str == "exit")
+			{
+				std::cout << "c ya" << std::endl;
+				break ;
+			}
+			else
+				std::cout << "Type ADD, SEARCH or EXIT" << std::endl;
+		}
 	}
 	else
 		std::cout << "error" << std::endl;
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttaneski <ttaneski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 14:39:17 by ttaneski          #+#    #+#             */
-/*   Updated: 2023/12/12 15:35:16 by ttaneski         ###   ########.fr       */
+/*   Updated: 2023/12/13 13:22:27 by ttaneski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,28 @@
 # define PHONEBOOK_HPP
 
 #include <iostream>
+#include <iomanip>
+
 class Contact
 {
-	public:
-	Contact(void);
-	~Contact(void);
+  public:
 	std::string first_name;
 	std::string last_name;
 	std::string nickname;
 	std::string darkest_secret;
-	int			phone_number;
+	std::string phone_number;
+	Contact(std::string first_name, std::string last_name, std::string nickname,
+		std::string darkest_secret, std::string phone_number);
+	~Contact(void);
 };
 
 class PhoneBook
 {
-	public:
-	std::string Contact[8];
+  public:
+	int contactCount;
+	Contact Contact[8];
 	PhoneBook(void);
 	~PhoneBook(void);
 };
-
 
 #endif
