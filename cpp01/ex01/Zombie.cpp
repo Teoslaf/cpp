c65506a31681692a3a48e6edc8e975d0ea484338
Teodor Taneski Feldschau <ttaneski@Nelas-MBP.fritz.box>
2023-12-24 16:09:05 +0100

    ex01

---
+++ cpp01/ex01/Zombie.cpp
@@ -0,0 +1,24 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name)
+{
+    this->name = name;
+	std::cout << name << " const with name" << std::endl;
+
+}
+
+Zombie::Zombie()
+{
+	std::cout << name << " const without name" << std::endl;
+	
+}
+
+Zombie::~Zombie(void)
+{
+	std::cout << name << " deconstr" << std::endl;
+}
+
+void	Zombie::announce(void)
+{
+	std::cout << name << ":"<< " BraiiiiiiinnnzzzZ..." << std::endl;
+}
---
+++ cpp01/ex01/Zombie.hpp
@@ -0,0 +1,21 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iomanip>
+# include <iostream>
+# include <cstdlib>
+
+class Zombie
+{
+  private:
+	std::string name;
+
+  public:
+	void announce( void );
+    Zombie(void);
+    Zombie(std::string name);
+		~Zombie(void);
+};
+Zombie* zombieHorde( int N, std::string name );
+void deletezobie(Zombie *horde);
+#endif
