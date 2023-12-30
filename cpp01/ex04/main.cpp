8e4baa2804c553d831c1b12898696cb35718b726
Teodor Taneski Feldschau <teotaneski1@gmail.com>
2023-12-30 12:15:57 +0100

    starting ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -2,5 +2,13 @@
 
 int main(int argc, char **argv)
 {
+	if(argc == 4)
+	{
+		std::cout << "filename :" << argv[1] << std::endl;
+		std::cout << "s1 :" << argv[2] << std::endl;
+		std::cout << "s2 :" << argv[3] << std::endl;
 
+	}
+	else
+		std::cout << "give me filename, s1 and s2" << std::endl;
 }
