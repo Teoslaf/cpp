bcfe36e088f619306733ac8377d34c44a84f0c02
Teodor Taneski Feldschau <teotaneski1@gmail.com>
2023-12-31 19:00:01 +0100

    done ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,36 +1,40 @@
 #include "main.hpp"
 
-void openSave(const std::string& filename, const std::string& s1, const std::string& s2)
+void openSave(const std::string &filename, const std::string &s1, const std::string &s2)
 {
 	(void)s2;
-		std::ifstream file(filename);
-		if(file.is_open())
+	std::ifstream file(filename);
+	if (file.is_open())
+	{
+		std::stringstream strStream;
+		strStream << file.rdbuf();
+		std::string str = strStream.str();
+		std::cout << str << std::endl;
+		size_t pos = str.find(s1);
+		while (pos != std::string::npos)
 		{
-			std::stringstream strStream;
-			strStream << file.rdbuf();
-			std::string str = strStream.str();
-			std::cout << str << std::endl;
-			size_t pos = str.find(s1);
-			std::cout << pos << std::endl;
-
+			str = str.substr(0, pos) + s2 + str.substr(pos + s1.length());
+			pos = str.find(s1, pos + s2.length());
 		}
+		std::ofstream outFile(filename + ".relace");
+		if(outFile.is_open())
+			outFile << str;
 		else
-			std::cout << "wrong filename" << std::endl;
-		file.close();
+		std::cout << "problem" << std::endl;
+	}
+	else
+		std::cout << "wrong filename" << std::endl;
+	file.close();
 }
 
 int main(int argc, char **argv)
 {
-	if(argc == 4)
+	if (argc == 4)
 	{
 		std::string filename = argv[1];
 		std::string s1 = argv[2];
 		std::string s2 = argv[3];
 		openSave(filename, s1, s2);
-		// std::cout << file << std::endl;
-		// std::ofstream file("relace");
-		// file << s1;
-		// file.close();
 	}
 	else
 		std::cout << "give me filename, s1 and s2" << std::endl;
