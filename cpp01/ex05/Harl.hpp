08bfd31bd4ac5be4c2d28175b13fa805d9200419
Teodor Taneski Feldschau <teotaneski1@gmail.com>
2023-12-31 20:49:14 +0100

    asd

--- cpp01/ex05/Harl.hpp
+++ cpp01/ex05/Harl.hpp
@@ -2,5 +2,15 @@
 # define HARL_HPP
 
 #include <iostream>
-
+class Harl
+{
+	private:
+	void debug( void );
+	void info( void );
+	void warning( void );
+	void error( void );
+	
+	public:
+	void complain( std::string level );
+};
 #endif
