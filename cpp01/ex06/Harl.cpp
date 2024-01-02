7f1ac78993c3393868e7e3619394849624588c2e
Teodor Taneski Feldschau <teotaneski1@gmail.com>
2024-01-02 17:06:12 +0100

    ready to push

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -33,19 +33,22 @@ void Harl::complain(std::string level)
 void Harl::debug(void)
 {
 	std::cout << "[ DEBUG ]" << std::endl;
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do! \n" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do! \n"
+			  << std::endl;
 }
 void Harl::info(void)
 {
 	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
-	std::cout << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n" << std::endl;
+	std::cout << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n"
+			  << std::endl;
 }
 void Harl::warning(void)
 {
 	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
-	std::cout << "I’ve been coming for years whereas you started working here since last month.\n" << std::endl;
+	std::cout << "I’ve been coming for years whereas you started working here since last month.\n"
+			  << std::endl;
 }
 void Harl::error(void)
 {
@@ -55,7 +58,6 @@ void Harl::error(void)
 Harl::Harl()
 {
 }
-
 Harl::~Harl()
 {
 }
--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -2,12 +2,13 @@
 
 int main(int argc, char **argv)
 {
-	if(argc != 2)
+	if (argc != 2)
 		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 	else
 	{
 		Harl Harl;
+		for (size_t i = 0; i < strlen(argv[1]); i++)
+			argv[1][i] = std::toupper(argv[1][i]);
 		Harl.complain(argv[1]);
 	}
-
 }
