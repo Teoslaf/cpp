a43bbf0c7b298617689f0e83efc27af527d41efc
Teodor Taneski Feldschau <teotaneski1@gmail.com>
2024-01-11 21:23:22 +0100

    a

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -7,29 +7,29 @@
 
 int main()
 {
-	Dog originalBrain;
-	originalBrain.setIdea(0, "Original Idea");
+	// Dog originalBrain;
+	// originalBrain.setIdea(0, "Original Idea");
 
-	Dog copyBrain = originalBrain;
-	copyBrain.setIdea(0, "Modified Copy Idea");
-	if (originalBrain.getIdea(0) == "Original Idea")
-	{
-		std::cout << "Deep copy test passed: Original is unchanged." << std::endl;
-	}
-	else
-	{
-		std::cout << "Shallow copy test failed: Original is modified." << std::endl;
-	}
-	originalBrain.setIdea(0, "Modified Original Idea");
+	// Dog copyBrain = originalBrain;
+	// copyBrain.setIdea(0, "Modified Copy Idea");
+	// if (originalBrain.getIdea(0) == "Original Idea")
+	// {
+	// 	std::cout << "Deep copy test passed: Original is unchanged." << std::endl;
+	// }
+	// else
+	// {
+	// 	std::cout << "Shallow copy test failed: Original is modified." << std::endl;
+	// }
+	// originalBrain.setIdea(0, "Modified Original Idea");
 
-	if (copyBrain.getIdea(0) == "Modified Copy Idea")
-	{
-		std::cout << "Deep copy test passed: Copy is unchanged." << std::endl;
-	}
-	else
-	{
-		std::cout << "Shallow copy test failed: Copy is modified." << std::endl;
-	}
+	// if (copyBrain.getIdea(0) == "Modified Copy Idea")
+	// {
+	// 	std::cout << "Deep copy test passed: Copy is unchanged." << std::endl;
+	// }
+	// else
+	// {
+	// 	std::cout << "Shallow copy test failed: Copy is modified." << std::endl;
+	// }
 
 	// if(org.getIdea(0) == "org Idea")
 	// {
