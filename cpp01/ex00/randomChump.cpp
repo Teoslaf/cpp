cffd1eca19352b05e6b4ef7832b132414717904c
Teodor Taneski Feldschau <ttaneski@studnet.42berlin.de>
2024-01-17 14:05:40 +0100

    push

--- cpp01/ex00/randomChump.cpp
+++ cpp01/ex00/randomChump.cpp
@@ -6,7 +6,7 @@
 /*   By: ttaneski <ttaneski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 11:05:16 by ttaneski          #+#    #+#             */
-/*   Updated: 2023/12/20 11:47:37 by ttaneski         ###   ########.fr       */
+/*   Updated: 2024/01/17 14:05:18 by ttaneski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void randomChump( std::string name )
 {
-	Zombie *Zombie = newZombie(name);
-	Zombie->announce();
-	delete Zombie;
+	Zombie zombie(name);
+	zombie.announce();
 }
