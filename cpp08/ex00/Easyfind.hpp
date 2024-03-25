7a693aed96aca00b8dd97a54b033ca91dc15d8d3
Teodor Taneski Feldschau <teotaneski1@gmail.com>
2024-03-25 15:08:30 +0100

    cpp08

--- cpp08/ex00/Easyfind.hpp
+++ cpp08/ex00/Easyfind.hpp
@@ -2,8 +2,20 @@
 #define _Easyfind_HPP_
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
+template <typename T>
 
+int easyfind(T& container, int value) {
+    typename T::iterator it;
 
+    it = std::find(container.begin(), container.end(), value);
 
+    if (it != container.end()) {
+		return std::distance(container.begin(), it);
+    } else {
+		return -1;
+    }
+}
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
-	std::cout << "lmao" << std::endl;
+	std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 2, 3};
+	int index = easyfind(vec, 3);
+	if(index == -1)
+		std::cout << "Element not found in the container." << std::endl;
+	else
+		std::cout << "Index of the element : "<< index << std::endl;
+
 }
