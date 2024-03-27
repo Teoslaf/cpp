092bf013a81512e7adbea08fe0de989a327af0e7
Teodor Taneski Feldschau <teotaneski1@gmail.com>
2024-03-27 17:34:25 +0100

    done

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -7,54 +7,62 @@
 #include <limits>
 #include <algorithm>
 #include <stack>
+#include <list>
+
 
 // ******************************************************** //
 //                         CLASSES                         //
 // ****************************************************** //
 
-template <typename T>
-
-class MutantStack
+template <typename T, typename Container = std::deque<T>>
+class MutantStack : public std::stack<T, Container>
 {
 public:
-	MutantStack(){};
-	MutantStack(MutantStack const &obj) : data(obj.data){};
-	~MutantStack(){};
-	MutantStack &operator=(const MutantStack &obj)
+	MutantStack() {}
+	MutantStack(const MutantStack &other) : std::stack<T, Container>(other)
 	{
-		this->data = obj.data;
+		*this = other;
+	}
+	~MutantStack() {}
+
+	MutantStack &operator=(const MutantStack &other)
+	{
+		if (this == &other)
+			return *this;
+		this->std::stack<T, Container>::operator=(other);
+		return *this;
 	}
 
 	bool empty() const
 	{
-		return data.empty();
+		return std::stack<T>::empty();
 	}
 	size_t size() const
 	{
-		return data.size();
+		return std::stack<T>::size();
 	}
-	void push(const T& t)
+	void push(const T &t)
 	{
-		data.push_back(t);
+		std::stack<T>::push(t);
 	}
 	void pop()
 	{
-		data.pop_back();
+		std::stack<T>::pop();
 	}
-	T& top()
+	T &top()
 	{
-		return data.back();
+		return std::stack<T>::top();	
 	}
-	class iterator
-	{
-		
-	};
-private:
-	std::vector<T> data;
-};
+	typedef typename std::stack<T, Container>::container_type::iterator iterator;
 
-// ******************************************************** //
-//                        FUNCTIONS                        //
-// ****************************************************** //
+	iterator begin()
+	{
+		return this->std::stack<T, Container>::c.begin();
+	}
+	iterator end()
+	{
+		return this->std::stack<T, Container>::c.end();
+	}
+};
 
 #endif
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -3,13 +3,25 @@
 int main()
 {
 	MutantStack<int> mstack;
-	std::stack<int> test;
-
 	mstack.push(5);
 	mstack.push(17);
-	mstack.push(20);
-	mstack.push(1);
-
-	std::cout << mstack.begin() << std::endl;
-
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
 }
