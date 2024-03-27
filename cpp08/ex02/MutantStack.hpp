fece3db83a875d94afdd7411505d5c3c2f7d6db9
Teodor Taneski Feldschau <teotaneski1@gmail.com>
2024-03-27 15:06:51 +0100

    ex02

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,9 +1,60 @@
-#ifndef _Easyfind_HPP_
-#define _Easyfind_HPP_
+#ifndef _MutantStack_HPP_
+#define _MutantStack_HPP_
 
 #include <iostream>
 #include <vector>
+#include <set>
+#include <limits>
 #include <algorithm>
+#include <stack>
 
+// ******************************************************** //
+//                         CLASSES                         //
+// ****************************************************** //
+
+template <typename T>
+
+class MutantStack
+{
+public:
+	MutantStack(){};
+	MutantStack(MutantStack const &obj) : data(obj.data){};
+	~MutantStack(){};
+	MutantStack &operator=(const MutantStack &obj)
+	{
+		this->data = obj.data;
+	}
+
+	bool empty() const
+	{
+		return data.empty();
+	}
+	size_t size() const
+	{
+		return data.size();
+	}
+	void push(const T& t)
+	{
+		data.push_back(t);
+	}
+	void pop()
+	{
+		data.pop_back();
+	}
+	T& top()
+	{
+		return data.back();
+	}
+	class iterator
+	{
+		
+	};
+private:
+	std::vector<T> data;
+};
+
+// ******************************************************** //
+//                        FUNCTIONS                        //
+// ****************************************************** //
 
 #endif
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -2,5 +2,14 @@
 
 int main()
 {
+	MutantStack<int> mstack;
+	std::stack<int> test;
+
+	mstack.push(5);
+	mstack.push(17);
+	mstack.push(20);
+	mstack.push(1);
+
+	std::cout << mstack.begin() << std::endl;
 
 }
